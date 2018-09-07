af7ff68fb78b5f2d80c0ec36fa1a8eb2fe4bdd8f
Chris <chrispanetta123@gmail.com>
2018-09-07 19:06:25 +1000

    added responsive nav  bar. removed bootstrap dependency.

--- js/personal.js
+++ js/personal.js
@@ -1,1 +1,14 @@
-/
+
+var hamburger = document.querySelector(".hamburger");
+var topNav = document.getElementById("myTopnav");
+
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
+    } else {
+    topNav.className = "topnav";
+    }
+});
