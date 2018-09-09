a28b6b07692a2758e428d5cd667a4978e89ac24d
Chris <chrispanetta123@gmail.com>
2018-09-09 20:16:26 +1000

    added jquery dependency. redid nav bar

--- js/personal.js
+++ js/personal.js
@@ -1,14 +1,11 @@
 
 var hamburger = document.querySelector(".hamburger");
-var topNav = document.getElementById("myTopnav");
 
-// On click
-hamburger.addEventListener("click", function() {
-  // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
-  if (topNav.className === "topnav") {
-    topNav.className += " responsive";
-    } else {
-    topNav.className = "topnav";
-    }
+$(document).ready(function(){
+
+  // On hamburger click
+  $(hamburger).on('click', function(){
+      hamburger.classList.toggle("is-active");
+      $('nav ul').toggleClass('show');
+  });
 });
